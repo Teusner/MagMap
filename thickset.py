321d2e4bd99fb935a80273532267fffa056a4fab
Brateau Quentin <quentin.brateau@ensta-bretagne.org>
2020-11-16 15:00:34 +0100

    Updating documentation

---
+++ thickset.py
@@ -0,0 +1,11 @@
+import numpy as np
+from pyibex import *
+from pyibex.thickset import *
+from tubex_lib import *
+
+
+if __name__ == "__main__":
+    vibes.beginDrawing()
+    vibes.newFigure('Path Exploration')
+    vibes.setFigureProperties(dict(x=0, y=10, width=500, height=500))
+    
